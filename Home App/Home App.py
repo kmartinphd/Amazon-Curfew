8dccfb6d137f9f917b2737ab24728e3cee4d8ea7
aqual <aqualcham@charter.net>
2017-09-08 23:55:40 -0400

    first commit version of Amazon home app

--- Home App/Home App.py
+++ Home App/Home App.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_ask import Ask, question, statement
-
+import Sensor
 
 app = Flask(__name__)
 ask = Ask(app, "/")
-
+mac_name_dict = {}
+macs_at_home = []
+blue_in_area = get_addresses()
+sensor = Sensor()
 @app.route("/")
 def homepage():
     """
@@ -13,3 +16,40 @@ def homepage():
     :return: message text to be posted
     """
     return render_template("index.html")
+
+@ask.launch()
+def start_skill():
+    return whoHome()
+
+@ask.intent("WhoHome")
+def run_who_hone():
+    return statement(whoHome())
+
+def whoHome():
+    global macs_at_home
+    count = 0
+    names = []
+    for m in macs_at_home:
+        count += 1
+        #MAP the address into the names list
+    message = "there are " + str(count) + " people home."
+    message += " These people are: "
+    for n in names:
+        message += ", " + n
+    return message
+
+
+def parseNameFile():
+    pass
+
+def get_addresses():
+    global macs_at_home
+    macs = sensor.get_mac_dict()
+    macs_at_home = []
+    for m in macs:
+        if macs[m] is not None:
+            macs_at_home.append(m)
+
+    return macs_at_home
+
+
---
+++ Home App/Sensor.py
@@ -0,0 +1,52 @@
+import os
+import json
+from threading import Lock
+from bt_proximity import BluetoothRSSI
+import time
+import thread
+import traceback
+import sys
+
+
+class Sensor:
+    def __init__(self):
+        self.dict_lock = Lock() #lock for the mac_dict variable
+        self.mac_dict = {} #dictionary mapping the bluetooth addresses to signal strength
+
+    def get_mac_dict(self):
+        return self.mac_dict
+
+    def update_dicts(self):
+        """
+        update the mac_dict with the latest set of bluetooth address
+        strengths
+        :return: None
+        """
+        while True:
+            try:
+                for m in self.mac_dict.keys():
+                    time.sleep(1)
+                    b= BluetoothRSSI(addr=m)
+                    rssi = b.get_rssi()
+                    self.dict_lock.release()
+                    self.mac_dict[m] = rssi
+                    self.dict_lock.release()
+            except (KeyboardInterrupt, SystemExit):
+                message = "Exception raised in Sensor run loop, method update_dict: either Keyboard Interrupt or System exit"
+                print message
+                self.dict_lock.release()
+                break
+
+            while True:
+                try:
+                    time.sleep(sys.maxint)
+                except KeyboardInterrupt, SystemExit:
+                    print "exit program"
+                    break
+
+    def run(self):
+        try:
+            thread.start_new_thread(self.update_dicts, ())
+        except:
+            message = "Failed to launch all threads in Sensor run method"
+            message += "Stack Trace:/n" + traceback.format_exc()
