e72e9773b8659bea075a4e2de40c1edbba9fcaa4
Stephen Eyerly <stepheneyerly@gmail.com>
2017-09-08 23:15:32 -0400

    Added information file

--- Home App/InformationStorage.py
+++ Home App/InformationStorage.py
@@ -2,6 +2,18 @@
 
 class InformationStorage:
 
+
     def __init__(self):
-        # You need to load the persons and place the data into an object
-        pass
+        self.person_list = []
+
+    def read_file(self):
+        file = open('information.text', 'r')
+
+        #file.read()
+
+
+class Person:
+
+    def __init__(self, name, mac_addresses):
+        self.name = name
+        self.mac_addresses = mac_addresses
