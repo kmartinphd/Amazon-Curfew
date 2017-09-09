32a2feb3002aae9d646f8c4c4624dcd2cb6b8394
Stephen Eyerly <stepheneyerly@gmail.com>
2017-09-08 22:59:02 -0400

    Created init and frame.

--- Home App/InformationStorage.py
+++ Home App/InformationStorage.py
@@ -1,1 +1,7 @@
 #This will handle the storage of Mac Address and the associated persons
+
+class InformationStorage:
+
+    def __init__(self):
+        # You need to load the persons and place the data into an object
+        pass
